84c2c05ff0db5e95c1be633a7d4548fbd283a22c
Matthias Morin <mat@tangoman.io>
2025-05-16 18:26:06 +0200

    feat: 🛸 2025-05-16 18:26:06

--- src/js/pivot/menubar-collapse.js
+++ src/js/pivot/menubar-collapse.js
@@ -4,18 +4,24 @@ import { closeButton } from "./close-button";
  * menubar collapse
  */
 export const initMenubarCollapse = () => {
-    const menubar = document.querySelector('nav [role="menubar"]');
-    if (!menubar) return;
-    
-    const button = closeButton();
-    menubar.appendChild(button);
+    const menubars = document.querySelectorAll('[role="menubar"]');
+    if (!menubars) return;
 
-    if (window.innerWidth < 768) {
-        menubar.setAttribute('aria-expanded', 'false');
-    }
+    menubars.forEach((menubar) => {
+        const button = closeButton();
+        if (menubar.firstChild) {
+            menubar.insertBefore(button, menubar.firstChild);
+        } else {
+            menubar.appendChild(button);
+        }
 
-    button.addEventListener('click', () => {
-        const isVisible = menubar.getAttribute('aria-expanded') !== 'false';
-        menubar.setAttribute('aria-expanded', !isVisible);
+        if (window.innerWidth < 768) {
+            menubar.setAttribute('aria-expanded', 'false');
+        }
+
+        button.addEventListener('click', () => {
+            const isVisible = menubar.getAttribute('aria-expanded') !== 'false';
+            menubar.setAttribute('aria-expanded', !isVisible);
+        });
     });
 };
